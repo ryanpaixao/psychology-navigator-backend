ad38dc7bcd8a67e66ca88e0ecb7c83cec10aa8b8
Ryan Paixao <16196485+ryanpaixao@users.noreply.github.com>
2025-06-04 13:49:57 -0300

    Create initial migration script. create_tables.py

---
+++ create_tables.py
@@ -0,0 +1,11 @@
+from database import Base, engine
+from models import Paper, Citation # Import all models
+
+def create_database():
+  # This will create all tables defined in models
+  Base.metadata.create_all(bind=engine)
+
+
+if __name__ == "__main__":
+  create_database()
+  print("Tables created successfully!")
